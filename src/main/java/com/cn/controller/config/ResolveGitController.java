27f9043c0821d66feac1817acec182bddf0c8d4d
Administrator <13911008254@139.com>
2019-09-05 18:03:32 +0800

    111

---
+++ src/main/java/com/cn/controller/config/ResolveGitController.java
@@ -0,0 +1,24 @@
+package com.cn.controller.config;
+
+/**
+ * @author huangqiming
+ * @解析Git
+ */
+import com.cn.controller.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ResolveGitController extends ParentController {
+
+    @Value("${address}") // git配置文件里的key
+    private String address;
+
+    public ResolveGitController() {
+    }
+
+    @RequestMapping(value = "/getGitInfo", method = RequestMethod.POST)
+    public String getGitInfo() {
+        return address;
+    }
+}
